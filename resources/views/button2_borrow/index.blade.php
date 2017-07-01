202c8df6b6eaf605e3144e546a10ea77200f40ea
Ru <duannru0922@gmail.com>
2017-07-01 17:41:38 +0800

    Input Modal

--- resources/views/button2_borrow/index.blade.php
+++ resources/views/button2_borrow/index.blade.php
@@ -39,7 +39,7 @@
 <div class="TopTitle">借用狀況</div>
 <div class="TableTop">
 	<table class="table" style="table-layout: fixed;">
-	<tr><th>租借序號<th>租借日期<th>班級<th>申請人姓名<th>借用物品<th>數量<th>抵押證件<th>授課教室<th>授課教師<th>狀態<th>編輯資料
+	<tr><th>租借序號<th>租借日期<th>班級<th>申請人<th>借用物品<th>數量<th>抵押證件<th>授課教室<th>授課教師<th>狀態<th>編輯資料
 	</table>
 	@foreach($miss as $mis)
 	<!-- <!DOCTYPE html>
@@ -55,8 +55,8 @@
 	</div>
 
 	<!-- Modal -->
-	  <div class="modal fade" id="myModal{{$mis->id}}" role="dialog">
-	    <div class="modal-dialog">
+	  <div class="modal fade" id="myModal{{$mis->id}}" role="dialog"  style="height: 600px;">
+	    <div class="modal-dialog" >
 	    
 	      <!-- Modal content-->
 	      <div class="modal-content">
@@ -70,17 +70,17 @@
 	          {{ csrf_field()}}
 	
 	<div class="EditInfo">
-	<table class="table" style="table-layout: fixed; text-align: left">
-	<tr><th>租借編號 : </th><th><textarea class="form-control" disabled >{{ $mis->id}} </textarea></th></tr>
-	<tr><th>租借日期 :</th> <th><textarea class="form-control" name="date">{{ $mis->date }}</textarea></th></th>
-	<tr><th>租借人班級 :</th><th> <textarea class="form-control" name="class">{{ $mis->class }}</textarea></th></tr>
-    <tr><th>租借人姓名 : </th><th> <textarea class="form-control" name="name">{{ $mis->name }}</textarea> </th></tr>
-    <tr><th>租借物品 :</th> <th> <textarea class="form-control" name="item">{{ $mis->item }}</textarea> </th></tr>
-    <tr><th>租借數量 :</th><th><textarea class="form-control" name="itemnum">{{ $mis->itemnum }}</textarea></th></tr>
-    <tr><th>抵押證件 :</th><th> <textarea class="form-control" name="license">{{ $mis->license }}</textarea></th></tr>
-    <tr><th>教室 :</th><th> <textarea class="form-control" name="classroom">{{ $mis->classroom }}</textarea></th></tr>
-    <tr><th>教師 :</th><th> <textarea class="form-control" name="teacher">{{ $mis->teacher }}</textarea></th></tr>
-    <tr><th>狀態 :</th><th> <textarea class="form-control" name="status">{{ $mis->status }}</textarea></th></tr>
+	<table class="table" style="table-layout: fixed; text-align: left; line-height: 10px;">
+	<tr><th>租借序號 : </th><th><input  class="form-control" type="text" disabled value="{{ $mis->id}}"> </th></tr>
+	<tr><th>租借日期 :</th> <th><input  class="form-control" type="date" name="date" value="{{ $mis->date }}"></th></th>
+	<tr><th>班級 :</th><th> <input  class="form-control" type="text" name="class" value="{{ $mis->class }}"></th></tr>
+    <tr><th>申請人 : </th><th> <input  class="form-control" type="text" name="name" value="{{ $mis->name }}"></th></tr>
+    <tr><th>借用物品 :</th> <th> <input  class="form-control" type="text" name="item" value="{{ $mis->item }}"> </th></tr>
+    <tr><th>數量 :</th><th><input  class="form-control" type="number" name="itemnum" value="{{ $mis->itemnum }}"></th></tr>
+    <tr><th>抵押證件 :</th><th> <input  class="form-control" type="text" name="license" value="{{ $mis->license }}"></th></tr>
+    <tr><th>授課教室 :</th><th> <input  class="form-control" type="text" name="classroom" value="{{ $mis->classroom }}"></th></tr>
+    <tr><th>授課教師 :</th><th> <input  class="form-control" type="text" name="teacher" value="{{ $mis->teacher }}"></th></tr>
+    <tr><th>狀態 :</th><th> <input  class="form-control" type="text" name="status" value="{{ $mis->status }}"></th></tr>
 </table>
 </div>
 	        </div>
