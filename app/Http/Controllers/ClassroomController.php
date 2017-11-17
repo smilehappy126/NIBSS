f98d82c62ab225da9545d106d4f5d8944e391f12
linhu <s9224914@gmail.com>
2017-11-17 16:17:45 +0800

    整理controller
    新增教室 用 ClassroomController
    固定教室預約(多筆新增/刪除) 用 LongcourseController

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Classroom;
 
-class WeekController extends Controller
+class ClassroomController extends Controller
 {
     /**
      * Display a listing of the resource.
