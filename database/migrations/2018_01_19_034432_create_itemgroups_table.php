9cf011d4afe666f38f2d302c08abb814f2a60120
Ru <duannru0922@gmail.com>
2018-01-19 20:40:42 +0800

    Rules & Violations 初始值修正完成

---
+++ database/migrations/2018_01_19_034432_create_itemgroups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemgroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itemgroups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('groupname')->nullable()->default('無資料');
+            $table->integer('groupitemnum')->nullable()->default(1);
+            $table->string('createuser')->nullable()->default('系統預設');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itemgroups');
+    }
+}
