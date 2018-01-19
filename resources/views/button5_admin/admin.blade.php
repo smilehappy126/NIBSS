a0060105944e99c3e4d1471fc672b99cd79331a6
solomonjoeykao <solomonjoeykao@gmail.com>
2018-01-19 10:48:16 +0800

    管理者專區測試

--- resources/views/button5_admin/admin.blade.php
+++ resources/views/button5_admin/admin.blade.php
@@ -2,6 +2,89 @@
 @section('title', '管理者模式')
 @section('css')
 <style type="text/css">
+
+.hovereffect {
+  width: 100%;
+  height: 100%;
+  float: left;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  cursor: default;
+  margin-top:15%;
+}
+
+.hovereffect .overlay {
+  position: absolute;
+  overflow: hidden;
+  width: 80%;
+  height: 80%;
+  left: 10%;
+  top: 10%;
+  border-bottom: 1px solid #FFF;
+  border-top: 1px solid #FFF;
+  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+  transition: opacity 0.35s, transform 0.35s;
+  -webkit-transform: scale(0,1);
+  -ms-transform: scale(0,1);
+  transform: scale(0,1);
+}
+
+.hovereffect:hover .overlay {
+  opacity: 1;
+  filter: alpha(opacity=100);
+  -webkit-transform: scale(1);
+  -ms-transform: scale(1);
+  transform: scale(1);
+}
+
+.hovereffect img {
+  display: block;
+  position: relative;
+  -webkit-transition: all 0.35s;
+  transition: all 0.35s;
+}
+
+.hovereffect:hover img {
+  filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feComponentTransfer color-interpolation-filters="sRGB"><feFuncR type="linear" slope="0.6" /><feFuncG type="linear" slope="0.6" /><feFuncB type="linear" slope="0.6" /></feComponentTransfer></filter></svg>#filter');
+  filter: brightness(0.6);
+  -webkit-filter: brightness(0.6);
+}
+
+.hovereffect h2 {
+  text-transform: uppercase;
+  text-align: center;
+  position: relative;
+  font-size: 17px;
+  background-color: transparent;
+  color: #FFF;
+  padding: 1em 0;
+  opacity: 0;
+  filter: alpha(opacity=0);
+  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+  transition: opacity 0.35s, transform 0.35s;
+  -webkit-transform: translate3d(0,-100%,0);
+  transform: translate3d(0,-100%,0);
+}
+
+.hovereffect a, .hovereffect p {
+  color: #FFF;
+  padding: 1em 0;
+  opacity: 0;
+  filter: alpha(opacity=0);
+  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+  transition: opacity 0.35s, transform 0.35s;
+  -webkit-transform: translate3d(0,100%,0);
+  transform: translate3d(0,100%,0);
+}
+
+.hovereffect:hover a, .hovereffect:hover p, .hovereffect:hover h2 {
+  opacity: 1;
+  filter: alpha(opacity=100);
+  -webkit-transform: translate3d(0,0,0);
+  transform: translate3d(0,0,0);
+}
+
 /*PC CSS Section*/
 @media screen and (min-width: 900px){
     .Mobilesection{
@@ -129,7 +212,7 @@
         </div>
         @if(Auth::check())
     		@if(Auth::user()->level === '管理員')
-    			<div class="content" style="position: relative;">
+<!--     			<div class="content" style="position: relative;">
     				<table class="table" style="border: 0px; height: 100%; table-layout: fixed; text-align: center;">
     					<tr>
     						<th>
@@ -159,7 +242,77 @@
                             </th>
                         </tr>
     				</table>
-    			</div>
+    			</div> -->
+
+<div class="col-md-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
+            <div class="overlay">
+                <h2>歷史紀錄</h2>
+                <p>
+                  <form>
+                    <button type="button" data-toggle="modal" data-target="#SearchModal">LINK HERE</button>
+                    </form>
+                </p>
+            </div>
+    </div>
+</div>
+
+<div class="col-md-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
+            <div class="overlay">
+                <h2>使用者清單</h2>
+                <p>
+                <form action=" {{asset('/admin/rule')}} " method="get" >{{ csrf_field() }}
+                     <button type="submit">LINK HERE</button>
+                </form>
+                </p>
+            </div>
+    </div>
+</div>
+
+<div class="col-md-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
+            <div class="overlay">
+                <h2>可借用物品</h2>
+                <p>
+                <form action=" {{asset('/admin/item')}} " method="get" >{{ csrf_field() }}
+                     <button type="submit">LINK HERE</button>
+                </form>
+                </p>
+            </div>
+    </div>
+</div>
+
+<div class="col-md-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
+            <div class="overlay">
+                <h2>編輯條例</h2>
+                <p>
+                <form action=" {{asset('/admin/rule')}} " method="get" >{{ csrf_field() }}
+                     <button type="submit">LINK HERE</button>
+                </form>
+                </p>
+            </div>
+    </div>
+</div>
+
+<div class="col-md-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
+            <div class="overlay">
+                <h2>違規次數上限</h2>
+                <p>
+                <form>{{ csrf_field() }}
+                     <button type="button" data-toggle="modal" data-target="#ViolationModal">LINK HERE</button>
+                </form>
+                </p>
+            </div>
+    </div>
+</div>
     		@endif
     	@endif
     	
@@ -209,6 +362,8 @@
                         </tr>
                     </table>
                 </div>
+
+
             @endif
         @endif
         
